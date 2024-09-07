01de05562f313f00d810dcdfecd056b77924e125
Joel-Lavia <joeldisidi8@gmail.com>
2024-09-07 03:39:55 +0100

    fix:first algo resolut

--- bin/algo_dart.dart
+++ bin/algo_dart.dart
@@ -1,5 +1,25 @@
 import 'package:algo_dart/algo_dart.dart' as algo_dart;
 
+//1.Crée une fonction qui prend en entrée une chaîne de caractères et retourne le nombre de voyelles qu'elle contient.
+
+int returnVoyelles(str){
+
+int result = 0;  
+
+List<String> voyelles = ["a", "e", "i", "o", "u", "y"];
+
+for (int i = 0; i < str.length; i++) {
+
+if (voyelles.contains(str[i])) {
+result ++;
+}
+}
+
+return result;
+}
+
+
+
 void main() {
-  
+print(returnVoyelles("motielyo"));
 }
